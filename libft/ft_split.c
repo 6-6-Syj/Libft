42a6a9c8d99a241071fe5546c042b03de5ee2b4f
6-6-Syj <magand.jessy@outlook.fr>
2024-11-20 17:51:50 +0100

    split fixed !

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -13,34 +13,83 @@
 #include "libft.h"
 #include <stdlib.h>
 
+#include <stdio.h>
 
-static int  compare(char s, char c)
-{
-    if (c == s[i])
-            return (1);
-    return (0);
-}
 
 static int  count_words(char *s, char c)
 {
     size_t  i;
     size_t  count;
+    size_t  word;
 
     i = 0;
     count = 0;
+    word = 0;
     while (s[i])
     {
-        if (compare(s, c))
+        if (s[i] != c && word == 0)
+        {
             count++;
+            word = 1;
+        }
+        else if (s[i] == c)
+            word = 0;
         i++;
     }
     return (count);
+}
+
+static char *fill(char *s, int start, int end)
+{
+    char    *word;
+    size_t  i;
+
+    i = 0;
+    word = malloc((end - start + 1) * sizeof(char));
+    if (!word)
+        return (NULL);
+    while (start < end)
+    {
+        word[i] = s[start];
+        start++;
+        i++;
+    }
+    word[i] = '\0';
+    return (word);
 }
 
 char    **ft_split(char const *s, char c)
 {
     char    **arr;
     size_t  i;
+    size_t  j;
+    int  word;
 
+    if (!s)
+    {
+        arr = ft_calloc(1, 1);
+        return (arr);
+    }
+    arr = malloc((count_words((char *) s, c) + 1) * sizeof(char *));
+    if (!arr)
+        return (NULL);
     i = 0;
+    j = 0;
+    word = -1;
+    while (i <= ft_strlen(s))
+    {
+        if (s[i] != c && word < 0)
+            word = i;
+        else if (((s[i] == c || i == ft_strlen(s))) && word >= 0)
+            {
+                arr[j] = fill((char *) s, word, i);
+                // if (!(arr[j]))
+                    // free
+                word = -1;
+                j++;
+            }
+            i++;
+    }
+    arr[j] = NULL;
+    return (arr);
 }
